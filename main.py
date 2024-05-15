9fad81cf335a91cc85b4f9d1a0587b14e31ee411
Hasan <hasan.dev04@gmail.com>
2024-05-15 16:29:29 +0600

    code formatted

--- main.py
+++ main.py
@@ -23,14 +23,18 @@ try:
         if cur_user is None:
             cur_user = auth(c_bank, Admin, User)
             isFirstTime = True
+
         else:
             isFirstTime = header(cur_user, isFirstTime)
+
             if cur_user.role == "user":
                 decoration(isFirstTime)
                 cur_user, isFirstTime = user_functionalities(c_bank, cur_user)
+
             elif cur_user.role == "admin":
                 decoration(isFirstTime)
                 cur_user, isFirstTime = admin_functionalities(c_bank, cur_user)
+
 except Exception as e:
     print("---------------------------------------")
     print(f"An error occurred: {type(e).__name__}")
